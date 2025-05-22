4974189f8686876ae24ae82897e0b4a12693bb42
Nikita Skryabin <nekit2271@gmail.com>
2025-05-22 10:44:54 +0300

    feat(entrypoint): add verbose mode configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,13 @@
 
 ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
 
-exec ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
+case "${SS_VERBOSE:-1}" in
+  0)
+    VERBOSE_FLAG=""
+    ;;
+  *)
+    VERBOSE_FLAG="-v"
+    ;;
+esac
+
+exec ss-server "${VERBOSE_FLAG}" -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
