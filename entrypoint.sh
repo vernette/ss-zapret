011f3d2f569a7b35cfe691831cd8f0170dfcd951
Nikita Skryabin <nekit2271@gmail.com>
2025-06-19 10:28:56 +0300

    refactor(entrypoint): refactor verbose control for ss-local and ss-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,9 @@
 
 /opt/zapret/init.d/sysv/zapret start
 
+if [ "${SS_VERBOSE:-1}" = "0" ]; then
+  exec >/dev/null 2>&1
+fi
+
 ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
-
-case "${SS_VERBOSE:-1}" in
-  0)
-    VERBOSE_FLAG=""
-    ;;
-  *)
-    VERBOSE_FLAG="-v"
-    ;;
-esac
-
-exec ss-server "${VERBOSE_FLAG}" -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
+exec ss-server -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
