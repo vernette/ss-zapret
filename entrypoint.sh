b605a80d8326aa1d10d4a91f3b284f4553f00dd1
Nikita Skryabin <nekit2271@gmail.com>
2025-10-05 18:24:26 +0300

    fix(entrypoint): enable verbose ss-server output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ if [ "${SS_VERBOSE:-1}" = "0" ]; then
 fi
 
 ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
-exec ss-server -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
+exec ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u
